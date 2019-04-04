d8cc4a92923a76ed496feb0b6631c60e6b26fd85
Simon Mittag <simon@sypht.com>
2019-04-04 11:12:20 +1100

    checking env vars

--- src/main/java/com/sypht/OAuthClient.java
+++ src/main/java/com/sypht/OAuthClient.java
@@ -19,9 +19,21 @@ import java.io.IOException;
 public class OAuthClient extends com.sypht.CloseableHttpClient {
     protected static String SYPHT_AUTH_ENDPOINT = "https://login.sypht.com/oauth/token";
     protected ResponseHandler<String> responseHandler;
+    protected String clientId;
+    protected String clientSecret;
+    protected String oauthAudience;
 
     public OAuthClient() {
         super();
+        clientId = System.getenv("OAUTH_CLIENT_ID");
+        clientSecret = System.getenv("OAUTH_CLIENT_SECRET");
+        if(clientId == null || clientSecret == null) {
+            throw new RuntimeException("OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET environment variables must be set before running this process, exiting");
+        }
+        oauthAudience = System.getenv("OAUTH_AUDIENCE");
+        if(oauthAudience==null) {
+            oauthAudience = "https://api.sypht.com";
+        }
         this.responseHandler = new ResponseHandler<String>() {
             @Override
             public String handleResponse(
@@ -45,9 +57,9 @@ public class OAuthClient extends com.sypht.CloseableHttpClient {
         httpPost.setHeader("Content-Type", "application/json");
 
         String json = "{" +
-                "\"client_id\":\"" + System.getenv("OAUTH_CLIENT_ID") + "\"," +
-                "\"client_secret\":\"" + System.getenv("OAUTH_CLIENT_SECRET") + "\"," +
-                "\"audience\":\"" + System.getenv("OAUTH_AUDIENCE") + "\"," +
+                "\"client_id\":\"" + clientId + "\"," +
+                "\"client_secret\":\"" + clientSecret + "\"," +
+                "\"audience\":\"" +  oauthAudience + "\"," +
                 "\"grant_type\":\"client_credentials\"" +
                 "}";
         StringEntity entity = new StringEntity(json);
