1506947b312937986ca40e5eb5fb5b95edf33b52
Rinku Yadav <rinku.yadav@bpay.com.au>
2019-06-18 16:15:52 +1000

    Added proxy config

--- src/main/java/com/sypht/CloseableHttpClient.java
+++ src/main/java/com/sypht/CloseableHttpClient.java
@@ -3,6 +3,7 @@
  */
 package com.sypht;
 
+import org.apache.http.HttpHost;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -17,10 +18,59 @@ public abstract class CloseableHttpClient {
 
         HttpClientConnectionManager poolingConnManager
                 = new PoolingHttpClientConnectionManager();
-        this.httpClient = HttpClients
-                .custom()
-                .setConnectionManager(poolingConnManager)
-                .build();
+
+        HttpHost proxy = getSystemProxy();
+        if (proxy!=null) {
+            this.httpClient = HttpClients
+                    .custom().setProxy(proxy)
+                    .setConnectionManager(poolingConnManager)
+                    .build();
+        } else {
+            this.httpClient = HttpClients
+                    .custom()
+                    .setConnectionManager(poolingConnManager)
+                    .build();
+        }
+    }
+
+    private HttpHost getSystemProxy() {
+        HttpHost proxy = null;
+        String proxyHost = null;
+        Integer proxyPort = null;
+        String httpProxyHost = System.getProperty("http.proxyHost");
+        String httpsProxyHost = System.getProperty("https.proxyHost");
+        String socksProxyHost = System.getProperty("socksProxyHost");
+
+        if (httpProxyHost != null) {
+            proxyHost = httpProxyHost;
+            String httpProxyPortStr = System.getProperty("http.proxyPort");
+            if (httpProxyPortStr == null) {
+                proxyPort = 80;
+            } else {
+                proxyPort = Integer.parseInt(httpProxyPortStr);
+            }
+        } else if (httpsProxyHost != null) {
+            proxyHost = httpsProxyHost;
+            String httpsProxyPortStr = System.getProperty("https.proxyPort");
+            if (httpsProxyPortStr == null) {
+                proxyPort = 443;
+            } else {
+                proxyPort = Integer.parseInt(httpsProxyPortStr);
+            }
+        } else if (socksProxyHost != null) {
+            proxyHost = socksProxyHost;
+            String socksProxyPortStr = System.getProperty("socksProxyPort");
+            if (socksProxyPortStr == null) {
+                proxyPort = 1080;
+            } else {
+                proxyPort = Integer.parseInt(socksProxyPortStr);
+            }
+        }
+
+        if (proxyHost != null && proxyPort != null) {
+            proxy = new HttpHost(proxyHost, proxyPort);
+        }
+        return proxy;
     }
 
     @Override
